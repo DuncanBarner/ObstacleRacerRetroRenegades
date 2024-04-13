7d8db93005508a018eaf641826987bb144836ce4
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2024-04-13 00:42:14 -0500

    Wall Jump & Slide
    +Wall jump is fully working ( we'll probably want to tweak how it works)
    
    +Added in some Prefabs and Scripts
    
    +Added in a test "animation" to test if certain parts of the slide were working
    
    =Slide code is in, but isn't fully working

---
+++ ObstacleRacer/Assets/Scripts/Player Related/Player Movement/PlatformPlayerController.cs
@@ -0,0 +1,168 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformPlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+   
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    
+    public float coyoteTime = 0.1f;
+    public float variableJumpHeightMultiplier = 0.5f;
+    public float fallingSpeed = 2f;
+
+    //wall jump
+    private bool isWallSliding;
+    private float wallSlidingSpeed = 2f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private float horizontalInput;
+    private bool isJumping;
+    private float jumpTimeCounter;
+
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpPower = new Vector2(8f, 16f);
+    
+    private Animator animator;
+    [SerializeField] private AudioClip jumpSoundClip;
+   
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        if (groundCheck == null)
+        {
+            Debug.LogError("GroundCheck not assigned to the player controller!");
+        }
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // Coyote Time: Allow jumping shortly after leaving the ground
+        if (isGrounded)
+        {
+            jumpTimeCounter = coyoteTime;
+        }
+        else
+        {
+            jumpTimeCounter -= Time.deltaTime;
+        }
+
+        // Check for jump input
+        if (Input.GetButtonDown("Jump") && (isGrounded || jumpTimeCounter > 0 && !OnWall()) && !PauseMenu.isPaused)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isJumping = true;
+            jumpTimeCounter = 0;
+            //SoundFXManager.instance.PlaySoundFXCLip(jumpSoundClip, transform, 1f);
+        }
+        WallJump();
+
+        // Variable Jump Height: Allow higher jumps by holding the jump button
+        if (Input.GetButtonUp("Jump") && isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
+            isJumping = false;
+        }
+        if (rb.velocity.y < 0 && !isGrounded)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallingSpeed - 1) * Time.deltaTime;
+        }
+
+        WallSlide();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        // Flip the player sprite based on movement direction
+        if (horizontalInput > 0 && !PauseMenu.isPaused)
+        {
+            transform.localScale = new Vector3(2f, 2f, 1f); // Facing right
+        }
+        else if (horizontalInput < 0 && !PauseMenu.isPaused)
+        {
+            transform.localScale = new Vector3(-2f, 2f, 1f); // Facing Left
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Visualize the ground check radius in the Scene view
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+    public bool OnWall()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    private void WallSlide()
+    {
+        if (OnWall() && !isGrounded &&horizontalInput!=0f )
+        {
+            isWallSliding = true;
+            rb.velocity  = new Vector2(rb.velocity.x,Mathf.Clamp(rb.velocity.y,-wallSlidingSpeed,float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    private void WallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if(Input.GetButtonDown("Jump") && wallJumpingCounter>0f)
+        {
+            isWallJumping = true;
+            rb.velocity = new Vector2(wallJumpingDirection * wallJumpPower.x, wallJumpPower.y);
+            wallJumpingCounter = 0f;
+
+            if(transform.localScale.x != wallJumpingDirection)
+            {
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+    public float getHorizontalInput() { return horizontalInput;}
+}
---
+++ ObstacleRacer/Assets/Scripts/Player Related/Player Movement/playerSlide.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSlide: MonoBehaviour
+{
+    public PlatformPlayerController pC;
+    public Rigidbody2D rb;
+    public Animator animator;
+    public CapsuleCollider2D regularColli;
+    public CapsuleCollider2D slideColli;
+    public float slideSpeed = 800f;
+    public bool isSliding = false;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !pC.OnWall())
+            slide();
+    }
+
+    private void slide()
+    {
+        isSliding = true;
+
+        animator.SetBool("isSliding", true);
+
+        regularColli .enabled = false;
+        slideColli .enabled = true;
+
+        if (pC.getHorizontalInput() > 0 && !PauseMenu.isPaused)
+            {
+             rb.AddForce(Vector2.right * slideSpeed); // Facing right
+            }
+        else
+            {
+             rb.AddForce(Vector2.left * slideSpeed); // Facing Left
+            }
+
+        StartCoroutine("stopSlide");
+    }
+
+    IEnumerator stopSlide()
+    {
+        yield return new WaitForSeconds(1.5f);
+        //animator.Play("");
+        animator.SetBool("isSliding", false);
+        regularColli.enabled = true;
+        slideColli.enabled = false;
+        isSliding = false;
+    }
+
+}
