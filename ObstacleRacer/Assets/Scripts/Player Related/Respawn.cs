5115ab504380793574731cf4b7be663ec5acdda9
Duncan Barner <duncan.barner@gmail.com>
2024-04-27 18:02:21 -0500

    Added some Animations

---
+++ ObstacleRacer/Assets/Scripts/Player Related/Respawn.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    public GameObject anchor;
+    public GameObject player;
+    
+
+    public void newAnchor(GameObject newAnchor)
+    {
+        anchor = newAnchor;
+    }
+
+    public void respawn()
+    {
+        //player.
+    }
+}
